7f5373302dfd0dff4826cf20ee81cad8542ff04c
IkennaOkafor <ikennajeshuaokafor@gmail.com>
2022-03-24 10:45:37 -0400

    added dev branch with code to use protocol with files instead of network

--- src/syscNetProto.cpp
+++ src/syscNetProto.cpp
@@ -2,9 +2,37 @@
 using namespace std;
 
 
+// static string prefix = string(getenv("WORKSPACE_PATH"));
+static string prefix = "/export/home/izo5011/WorkSpace/";
+
+
 int send_message(int socket, msgHeader_t* hdr, uint8_t* blk)
 {
     int ret;
+#ifdef MODEL_TECH
+    FILE* fd;
+    string buf_path = prefix + "/SYSC_FPGA/simulation/" + to_string(hdr->msgType) + ".bin";
+    fd = fopen(buf_path.c_str(), "w");
+    ret = fwrite(hdr, 1, sizeof(msgHeader_t), fd);
+    fflush(fd);
+    fclose(fd);
+    buf_path = prefix + "/SYSC_FPGA/simulation/" + to_string(hdr->msgType) + "_done.bin";
+    fd = fopen(buf_path.c_str(), "w");
+    fflush(fd);
+    fclose(fd);
+    if(hdr->pyld)
+    {
+        string buf_path = prefix + "/SYSC_FPGA/simulation/" + to_string(hdr->msgType) + "_pyld.bin";
+        fd = fopen(buf_path.c_str(), "w");
+        ret = fwrite(blk, 1, hdr->length, fd);
+        fflush(fd);
+        fclose(fd);
+        buf_path = prefix + "/SYSC_FPGA/simulation/" + to_string(hdr->msgType) + "_pyld_done.bin";
+        fd = fopen(buf_path.c_str(), "w");
+        fflush(fd);
+        fclose(fd);
+    }
+#else
     msgHeader_t hdr_tmp;
     // Convert to the network format
     hdr_tmp.msgType = (msgType_t)htonl(hdr->msgType);
@@ -15,6 +43,7 @@ int send_message(int socket, msgHeader_t* hdr, uint8_t* blk)
     {
 	    ret = send_data(socket, blk, hdr->length);
     }
+#endif
     return ret;
 }
 
@@ -22,6 +51,41 @@ int send_message(int socket, msgHeader_t* hdr, uint8_t* blk)
 int recv_message(int socket, msgHeader_t* hdr, uint8_t* blk)
 {
     int ret;
+#ifdef MODEL_TECH
+    FILE* fd;
+    string buf_path = prefix + "/SYSC_FPGA/simulation/" + to_string(hdr->msgType) + "_done.bin";
+    struct stat buffer;
+    while(stat(buf_path.c_str(), &buffer));
+    if(remove(buf_path.c_str()))
+    {
+        perror("Error deleting file");
+    }
+    buf_path = prefix + "/SYSC_FPGA/simulation/" + to_string(hdr->msgType) + ".bin";
+    fd = fopen(buf_path.c_str(), "r");
+    ret = fread(hdr, 1, sizeof(msgHeader_t), fd);
+    fclose(fd);
+    if(remove(buf_path.c_str()))
+    {
+        perror("Error deleting file");
+    }    
+    if(hdr->pyld)
+    {
+        string buf_path = prefix + "/SYSC_FPGA/simulation/" + to_string(hdr->msgType) + "_pyld_done.bin";
+        while(stat(buf_path.c_str(), &buffer));
+        if(remove(buf_path.c_str()))
+        {
+            perror("Error deleting file");
+        }
+        buf_path = prefix + "/SYSC_FPGA/simulation/" + to_string(hdr->msgType) + "_pyld.bin";
+        fd = fopen(buf_path.c_str(), "r");        
+        ret = fread(blk, 1, hdr->length, fd);
+        fclose(fd);
+        if(remove(buf_path.c_str()))
+        {
+            perror("Error deleting file");
+        }
+    }
+#else
     // Read the message header
 	ret = recv_data(socket, (uint8_t*)hdr, sizeof(msgHeader_t), sizeof(msgHeader_t));
 	hdr->msgType = (msgType_t)ntohl(hdr->msgType);
@@ -31,6 +95,7 @@ int recv_message(int socket, msgHeader_t* hdr, uint8_t* blk)
     {
 		ret = recv_data(socket, blk, hdr->length, hdr->length);
     }
+#endif
 	return ret;
 }
 
@@ -38,11 +103,16 @@ int recv_message(int socket, msgHeader_t* hdr, uint8_t* blk)
 int wait_message(int socket, msgHeader_t* hdr, uint8_t* blk, msgType_t mt)
 {
     int ret;
+#ifdef MODEL_TECH
+    hdr->msgType = mt;
+    ret = recv_message(socket, hdr, blk);
+#else
 	do
     {
 		hdr->msgType = NULL_MSG;
 	    ret = recv_message(socket, hdr, blk);
     } while(hdr->msgType != mt);
 	// Return succesfully
+#endif
 	return ret;
 }
